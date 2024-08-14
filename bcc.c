f87075281508403a06e29d1a40bb2edd49cd5191
Zane Schaffer <znschaffer@gmail.com>
2024-08-14 09:33:37 -0700

    clean up code style
    seperate out the jump list into it's own function

--- bcc.c
+++ bcc.c
@@ -3,127 +3,140 @@
 #include <stdlib.h>
 #include <string.h>
 
-int dataSize = 30000;
-int instructionSize = 200;
+int data_size = 30000;
+int instruction_size = 200;
+int instruction_count;
+int *jumps;
+
+char *instructions;
+char *data;
 
 int dp = 0;
 int ip = 0;
 
-void debugPrint(char instruction, char *data, int dp, int ip) {
+static void debug_print(char instruction, char *data, int dp, int ip)
+{
 #ifdef DEBUG
-  if (DEBUG) {
-    printf("Instruction: %c; dp: %d; data[dp]: %d; ip: %d\n", instruction, dp,
-           data[dp], ip);
-  }
+	if (DEBUG) {
+		printf("Instruction: %c; dp: %d; data[dp]: %d; ip: %d\n",
+		       instruction, dp, data[dp], ip);
+	}
 #endif
 }
 
-void upsize(void **arr, int *curSize, int neededSize) {
-  if (neededSize == *curSize) {
-    void *tmp = realloc(*arr, 2 * *curSize * sizeof(*arr));
-    if (tmp) {
-      *arr = tmp;
-      *curSize *= 2;
-    }
-  }
+static void build_jump_list(void)
+{
+	jumps = malloc(instruction_count);
+	memset(jumps, -1, instruction_count);
+
+	int jump_stack[instruction_count];
+	int p_count = 0;
+
+	for (int offset = 0; offset < instruction_size; offset++) {
+		if (instructions[offset] == '[') {
+			p_count += 1;
+			jump_stack[p_count - 1] = offset;
+		} else if (instructions[offset] == ']') {
+			p_count -= 1;
+			int match = jump_stack[p_count];
+			jumps[offset] = match;
+			jumps[match] = offset;
+			jump_stack[p_count] = 0;
+		}
+	}
 }
 
-int main(int argc, char **argv) {
-  if (argc == 1) {
-    fprintf(stdout, "usage: bcc filename.bf\n");
-    return 1;
-  }
+static void upsize(void **arr, int *curSize, int neededSize)
+{
+	while (neededSize >= *curSize) {
+		void *tmp = realloc(*arr, 2 * *curSize * sizeof(*arr));
+		if (tmp) {
+			*arr = tmp;
+			*curSize *= 2;
+		}
+	}
+}
 
-  char *data = calloc(dataSize, sizeof(char));
-  char *instructions = calloc(instructionSize, sizeof(char));
-  int c;
+int main(int argc, char **argv)
+{
+	if (argc == 1) {
+		fprintf(stdout, "usage: bcc filename.bf\n");
+		return 1;
+	}
 
-  FILE *file = fopen(argv[1], "r");
-  int instructionCount = 0;
+	data = calloc(data_size, sizeof(char));
+	instructions = calloc(instruction_size, sizeof(char));
+	int c;
 
-  if (file) {
-    while ((c = getc(file)) != EOF) {
-      if (c != '[' && c != ']' && c != '>' && c != '<' && c != '+' &&
-          c != '-' && c != '.' && c != ',') {
-        continue;
-      }
-      upsize((void **)&instructions, &instructionSize, instructionCount);
-      instructions[instructionCount++] = c;
-    }
-    fclose(file);
-  } else {
-    fprintf(stderr, "Couldn't open file: %s\n", argv[1]);
-    return 1;
-  }
+	FILE *file;
+	if ((file = fopen(argv[1], "r")) == NULL) {
+		fprintf(stderr, "Couldn't open file: %s\n", argv[1]);
+		return 1;
+	}
 
-  // build parentheses relationships
-  int parenRelations[instructionCount];
-  memset(parenRelations, 0, instructionCount);
+	while ((c = getc(file)) != EOF) {
+		if (c != '[' && c != ']' && c != '>' && c != '<' && c != '+' &&
+		    c != '-' && c != '.' && c != ',') {
+			continue;
+		}
+		upsize((void **)&instructions, &instruction_size,
+		       instruction_count);
+		instructions[instruction_count++] = c;
+	}
 
-  int parenStack[instructionCount];
-  int pCount = 0;
-  int tp = 0;
+	fclose(file);
 
-  while (tp < instructionCount) {
-    if (instructions[tp] == '[') {
-      pCount += 1;
-      parenStack[pCount - 1] = tp;
-    } else if (instructions[tp] == ']') {
-      pCount -= 1;
-      int matchTP = parenStack[pCount];
-      parenRelations[matchTP] = tp;
-      parenRelations[tp] = matchTP;
-      parenStack[pCount] = 0;
-    }
-    tp += 1;
-  }
+	build_jump_list();
 
-  while (ip <= instructionCount) {
-    uint8_t instruction = instructions[ip];
-    switch (instruction) {
-      case '>':
-        if (dp < dataSize) dp += 1;
-        debugPrint('>', data, dp, ip);
-        break;
-      case '<':
-        if (dp > 0) dp -= 1;
-        debugPrint('<', data, dp, ip);
-        break;
-      case '+':
-        data[dp] += 1;
-        debugPrint('+', data, dp, ip);
-        break;
-      case '-':
-        data[dp] -= 1;
-        debugPrint('-', data, dp, ip);
-        break;
-      case '.':
-        debugPrint('.', data, dp, ip);
-        putchar(data[dp]);
-        break;
-      case ',':
-        debugPrint(',', data, dp, ip);
-        scanf("%c", &data[dp]);
-        break;
-      case '[': {
-        debugPrint('[', data, dp, ip);
-        if (data[dp] == 0) {
-          ip = parenRelations[ip];
-        }
-      } break;
-      case ']': {
-        debugPrint(']', data, dp, ip);
-        if (data[dp] != 0) {
-          ip = parenRelations[ip];
-        }
-      } break;
-    }
-    ip += 1;
-  }
+	while (ip <= instruction_count) {
+		uint8_t instruction = instructions[ip];
+		switch (instruction) {
+		case '>':
+			if (dp < data_size)
+				dp += 1;
+			debug_print('>', data, dp, ip);
+			break;
+		case '<':
+			if (dp > 0)
+				dp -= 1;
+			debug_print('<', data, dp, ip);
+			break;
+		case '+':
+			data[dp] += 1;
+			debug_print('+', data, dp, ip);
+			break;
+		case '-':
+			data[dp] -= 1;
+			debug_print('-', data, dp, ip);
+			break;
+		case '.':
+			debug_print('.', data, dp, ip);
+			putchar(data[dp]);
+			break;
+		case ',':
+			debug_print(',', data, dp, ip);
+			scanf("%c", &data[dp]);
+			break;
+		case '[': {
+			debug_print('[', data, dp, ip);
+			if (data[dp] == 0) {
+				ip = jumps[ip];
+			}
+		} break;
+		case ']': {
+			debug_print(']', data, dp, ip);
+			if (data[dp] != 0) {
+				ip = jumps[ip];
+			}
+		} break;
+		}
+		ip += 1;
+	}
 
-  free(instructions);
-  free(data);
+	free(instructions);
+	free(data);
+	free(jumps);
 
-  printf("\n");
-  return 0;
+	printf("\n");
+	return 0;
 }
